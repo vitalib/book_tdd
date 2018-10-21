4efc5b9229bed2c85d35946a4fe44d2710e69878
Vitali Baranov <vitali.baranov@gmail.com>
2018-10-21 19:13:51 +0300

    Add list/new simplify home

--- lists/tests.py
+++ lists/tests.py
@@ -11,8 +11,10 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+class NewListTest(TestCase):
+
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/', data={
+        self.client.post('/lists/new', data={
             'item_text': 'A new list item'
         })
         self.assertEqual(Item.objects.count(), 1)
@@ -20,17 +22,10 @@ class HomePageTest(TestCase):
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/', data={
+        response = self.client.post('/lists/new', data={
             'item_text': 'A new list item'
         })
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'],
-            '/lists/the-only-list-in-the-world/'
-        )
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 
 class ListViewTest(TestCase):
